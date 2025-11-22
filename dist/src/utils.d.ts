d3774bf42e80e2dad4af3dfd7cf9ebe160cf68f9
emeraldlinks <josephchukwumachristopher@gmail.com>
2025-11-22 02:17:24 +0100

    Remove node_modules and dist from tracking

--- dist/src/utils.d.ts
+++
@@ -1,5 +0,0 @@
-export interface FieldMeta {
-    type: string;
-    meta?: Record<string, string | boolean>;
-}
-export declare function tsTypeToSqlType(field: string | FieldMeta): "TEXT" | "INTEGER" | "BOOLEAN" | "DATETIME";
